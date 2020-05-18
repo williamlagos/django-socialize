89cbb422632b2e34e2a64d1a101687d53af5a993
William Oliveira de Lagos <william.lagos@icloud.com>
2020-05-17 22:11:19 -0300

    Removed old demo folder to start a new one

--- demo/demo/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from models import *
-
-admin.site.register(Event)
-admin.site.register(Project)
-admin.site.register(Interest)
-admin.site.register(Movement)
-admin.site.register(Promoted)
