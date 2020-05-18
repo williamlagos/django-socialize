960acbd80d3890a5887c18ac67ac652590da9448
William Oliveira de Lagos <william.lagos@icloud.com>
2020-05-17 22:08:38 -0300

    Phased out old views, added methods into one class-based view for now

--- socialize/views.py
+++ socialize/views.py
@@ -21,123 +21,124 @@
 from django.http import JsonResponse
 from django.views import View
 
-from .social import Search,Follows,ID,Deletes,Authentication,Twitter,Facebook,Tutorial,Coins
+from .services import *
 
 class AccountsView(View):
+
     def get(self, request):
         return JsonResponse({'accounts': 'success'})
 
-def discharge(request):
-    c = Coins()
-    if request.method == 'GET':
-        c.discharge(request)
+    def discharge(self, request):
+        c = Coins()
+        if request.method == 'GET':
+            c.discharge(request)
 
-def recharge(request):
-    c = Coins()
-    if request.method == 'GET':
-        c.recharge(request)
+    def recharge(self, request):
+        c = Coins()
+        if request.method == 'GET':
+            c.recharge(request)
 
-def balance(request):
-    c = Coins()
-    if request.method == 'GET':
-        c.balance(request)
+    def balance(self, request):
+        c = Coins()
+        if request.method == 'GET':
+            c.balance(request)
 
-def payment(request):
-    pay = Coins()
-    if request.method == 'GET':
-        return pay.view_recharge(request)
-    elif request.method == 'POST':
-        return pay.update_credit(request)
+    def payment(self, request):
+        pay = Coins()
+        if request.method == 'GET':
+            return pay.view_recharge(request)
+        elif request.method == 'POST':
+            return pay.update_credit(request)
 
-def search(request):
-    s = Search()
-    if request.method == 'GET':
-        return s.explore(request)
+    def search(self, request):
+        s = Search()
+        if request.method == 'GET':
+            return s.explore(request)
 
-def explore(request):
-    p = Profiles()
-    if request.method == 'GET':
-        return p.view_userinfo(request)
+    def explore(self, request):
+        p = Profiles()
+        if request.method == 'GET':
+            return p.view_userinfo(request)
 
-def activity(request):
-    p = Profiles()
-    if request.method == 'GET':
-        return p.view_activity(request)
+    def activity(self, request):
+        p = Profiles()
+        if request.method == 'GET':
+            return p.view_activity(request)
 
-def following(request):
-    fav = Follows()
-    if request.method == 'GET':
-        return fav.view_following(request)
+    def following(self, request):
+        fav = Follows()
+        if request.method == 'GET':
+            return fav.view_following(request)
 
-def follow(request):
-    f = Follows()
-    if request.method == 'GET':
-        return f.become_follower(request)
+    def follow(self, request):
+        f = Follows()
+        if request.method == 'GET':
+            return f.become_follower(request)
 
-def unfollow(request):
-    f = Follows()
-    if request.method == 'GET':
-        return f.leave_follower(request)
+    def unfollow(self, request):
+        f = Follows()
+        if request.method == 'GET':
+            return f.leave_follower(request)
 
-def ids(request):
-    i = ID()
-    if request.method == 'GET':
-        return i.view_id(request)
-    elif request.method == 'POST':
-        return i.finish_tutorial(request)
+    def ids(self, request):
+        i = ID()
+        if request.method == 'GET':
+            return i.view_id(request)
+        elif request.method == 'POST':
+            return i.finish_tutorial(request)
 
-def delete(request):
-    d = Deletes()
-    if request.method == 'GET':
-        return d.delete_element(request)
+    def delete(self, request):
+        d = Deletes()
+        if request.method == 'GET':
+            return d.delete_element(request)
 
-def profile(request):
-    prof = Profiles()
-    if request.method == 'GET':
-        return prof.view_profile(request)
-    elif request.method == 'POST':
-        return prof.update_profile(request)
+    def profile(self, request):
+        prof = Profiles()
+        if request.method == 'GET':
+            return prof.view_profile(request)
+        elif request.method == 'POST':
+            return prof.update_profile(request)
 
-def authenticate(request):
-    a = Authentication()
-    if request.method == 'GET':
-        return a.authenticate(request)
+    def authenticate(self, request):
+        a = Authentication()
+        if request.method == 'GET':
+            return a.authenticate(request)
 
-def leave(request):
-    a = Authentication()
-    if request.method == 'GET':
-        return a.leave(request)
+    def leave(self, request):
+        a = Authentication()
+        if request.method == 'GET':
+            return a.leave(request)
 
-def twitter_post(request):
-    t = Twitter()
-    if request.method == 'GET':
-        return t.update_status(request)
+    def twitter_post(self, request):
+        t = Twitter()
+        if request.method == 'GET':
+            return t.update_status(request)
 
-def facebook_post(request):
-    f = Facebook()
-    if request.method == 'GET':
-        return f.update_status(request)
+    def facebook_post(self, request):
+        f = Facebook()
+        if request.method == 'GET':
+            return f.update_status(request)
 
-def facebook_event(request):
-    f = Facebook()
-    if request.method == 'GET':
-        return f.send_event(request)
-    
-def facebook_eventcover(request):
-    f = Facebook()
-    if request.method == 'GET':
-        return f.send_event_cover(request)
-    
-def participate(request):
-    a = Authentication()
-    if request.method == 'GET':
-        return a.view_register(request)
-    elif request.method == 'POST':
-        return a.participate(request)
+    def facebook_event(self, request):
+        f = Facebook()
+        if request.method == 'GET':
+            return f.send_event(request)
+        
+    def facebook_eventcover(self, request):
+        f = Facebook()
+        if request.method == 'GET':
+            return f.send_event_cover(request)
+        
+    def participate(self, request):
+        a = Authentication()
+        if request.method == 'GET':
+            return a.view_register(request)
+        elif request.method == 'POST':
+            return a.participate(request)
 
-def tutorial(request):
-    t = Tutorial()
-    if request.method == 'GET':
-        return t.view_tutorial(request)
-    elif request.method == 'POST':
-        return t.finish_tutorial(request)
+    def tutorial(self, request):
+        t = Tutorial()
+        if request.method == 'GET':
+            return t.view_tutorial(request)
+        elif request.method == 'POST':
+            return t.finish_tutorial(request)
