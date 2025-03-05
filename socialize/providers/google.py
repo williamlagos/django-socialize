5dde044340548849ba8838e98a55f79c9952c997
William Oliveira de Lagos <william.lagos@icloud.com>
2025-03-05 11:13:25 +0100

    chore: removed old invalid developer api key

--- socialize/providers/google.py
+++ socialize/providers/google.py
@@ -28,14 +28,15 @@ try:
 except ImportError as e:
     pass
 
-#apis = json.load(open('settings.json','r'))
-#google_api = apis['social']['google']
-google_api = ('','')
+# apis = json.load(open('settings.json','r'))
+# google_api = apis['social']['google']
+google_api = ('', '')
+
 
 class GoogleSocialProvider:
 
     def __init__(self):
-        self.developer_key = "AI39si7wyQ0h6KhpWLhZfJa-U4mU65rO3Dj-05grmYkZk-kn_sv8br5UdDIEORwG-itcRn5wSBTFbgu02KyR_FhSQNaR0QbvSQ"
+        self.developer_key = settings.GOOGLE_API_KEY
         self.client_id = google_api['client_id']
 
     def videos_by_user(self, username):
@@ -44,36 +45,43 @@ class GoogleSocialProvider:
 
     def video_thumbnail(self, token, access_token):
         actoken = self.refresh_google_token(access_token)
-        uri = 'https://gdata.youtube.com/feeds/api/users/default/uploads/%s?access_token=%s' % (token,actoken)
-        thumbnail = parseString(self.do_request(uri)).getElementsByTagName('media:thumbnail')[0].attributes['url'].value
+        uri = 'https://gdata.youtube.com/feeds/api/users/default/uploads/%s?access_token=%s' % (
+            token, actoken)
+        thumbnail = parseString(self.do_request(uri)).getElementsByTagName(
+            'media:thumbnail')[0].attributes['url'].value
         return str(thumbnail)
 
     def video_entry(self, title, description, keywords, access_token):
         media_group = gdata.media.Group(title=gdata.media.Title(text=title),
-                                        description=gdata.media.Description(description_type='plain',text=description),
-                                        keywords=gdata.media.Keywords(text='efforia'),
+                                        description=gdata.media.Description(
+                                            description_type='plain', text=description),
+                                        keywords=gdata.media.Keywords(
+                                            text='efforia'),
                                         category=[gdata.media.Category(text='Entertainment',
                                                                        scheme='http://gdata.youtube.com/schemas/2007/categories.cat',
                                                                        label='Entertainment')],
                                         player=None)
-                                        #private=gdata.media.Private())
-        video_entry = gdata.youtube.YouTubeVideoEntry(media=media_group) 
-        url,token = self.get_upload_token(video_entry,access_token)
-        return url,token
-    
+        # private=gdata.media.Private())
+        video_entry = gdata.youtube.YouTubeVideoEntry(media=media_group)
+        url, token = self.get_upload_token(video_entry, access_token)
+        return url, token
+
     def get_upload_token(self, video_entry, access_token):
         actoken = self.refresh_google_token(access_token)
         print(actoken)
         headers = {
-                   'Authorization': 'OAuth %s' % actoken,
-                   'X-gdata-key': 'key=%s' % self.developer_key,
-                   'Content-type': 'application/atom+xml'
+            'Authorization': 'OAuth %s' % actoken,
+            'X-gdata-key': 'key=%s' % self.developer_key,
+            'Content-type': 'application/atom+xml'
         }
-        response = self.do_request('https://gdata.youtube.com/action/GetUploadToken',str(video_entry),headers)
+        response = self.do_request(
+            'https://gdata.youtube.com/action/GetUploadToken', str(video_entry), headers)
         print(response)
-        url = parseString(response).getElementsByTagName('url')[0].childNodes[0].data
-        token = parseString(response).getElementsByTagName('token')[0].childNodes[0].data
-        return url,token
-        
+        url = parseString(response).getElementsByTagName(
+            'url')[0].childNodes[0].data
+        token = parseString(response).getElementsByTagName(
+            'token')[0].childNodes[0].data
+        return url, token
+
     def search_video(self, search_terms):
         pass
