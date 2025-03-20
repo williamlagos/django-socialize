1c561a9b092eb1fb4b147d0bf96a7bc742ec9c17
William Oliveira de Lagos <william.lagos@icloud.com>
2025-03-20 15:59:08 +0100

    feat: improved setup script and renewed requirements for it

--- setup.py
+++ setup.py
@@ -1,14 +1,56 @@
+"""Setup script for the django-socialize app."""
+
 import os
-from setuptools import setup
+
+from setuptools import setup, Command
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+
+class DjangoTestCommand(Command):
+    """Custom command to run Django tests."""
+
+    description = 'Run Django tests.'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """Run the test suite."""
+        import django
+        from django.conf import settings
+        from django.core.management import execute_from_command_line
+
+        # Configure Django settings for testing
+        settings.configure(
+            INSTALLED_APPS=[
+                'django.contrib.auth',
+                'django.contrib.contenttypes',
+                'socialize',
+            ],
+            DATABASES={
+                'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'NAME': ':memory:',
+                }
+            },
+        )
+        django.setup()
+
+        # Run tests
+        execute_from_command_line(['manage.py', 'test', 'socialize.tests'])
+
+
 setup(
     name='django-socialize',
-    version='0.5.4',
+    version='0.6.0',
     packages=['socialize'],
     include_package_data=True,
     license='LGPLv3 License',
@@ -22,7 +64,6 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
@@ -30,4 +71,5 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
+    cmdclass={'test': DjangoTestCommand},
 )
